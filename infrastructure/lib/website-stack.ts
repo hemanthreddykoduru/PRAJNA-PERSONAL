6853793afb3e9b1d9ce7d0704bfb42aaf2761892
Hemanth Reddy <hemanthreddykoduru2005@gmail.com>
2026-05-08 11:35:28 +0530

    ci: fix cloudfront root object and spa routing

--- infrastructure/lib/website-stack.ts
+++ infrastructure/lib/website-stack.ts
@@ -3,49 +3,78 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class WebsiteStack extends cdk.Stack {
-  public readonly websiteBucket: s3.Bucket;
-  public readonly distribution: cloudfront.Distribution;
-
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // 1. Create the S3 Bucket for Website Hosting
-    this.websiteBucket = new s3.Bucket(this, 'PrajnaWebsiteBucket', {
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    // 1. Static Website Bucket
+    const websiteBucket = new s3.Bucket(this, 'PrajnaWebsiteBucketFinal', {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
     });
 
-    // 2. Create CloudFront Distribution (OAI)
-    this.distribution = new cloudfront.Distribution(this, 'PrajnaDistribution', {
+    // 2. Origin Access Control (OAC)
+    const oac = new cloudfront.CfnOriginAccessControl(this, 'PrajnaWebsiteOACFinal', {
+      originAccessControlConfig: {
+        name: 'Prajna Website OAC Final',
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4',
+      },
+    });
+
+    // 3. CloudFront Distribution
+    const distribution = new cloudfront.Distribution(this, 'PrajnaWebsiteDistributionFinal', {
       defaultBehavior: {
-        origin: new origins.S3Origin(this.websiteBucket),
+        origin: new origins.S3Origin(websiteBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
       },
-      defaultRootObject: 'index.html',
+      defaultRootObject: 'index.html', // <--- THE CRITICAL FIX
       errorResponses: [
+        {
+          httpStatus: 403,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html', // For SPA routing
+          ttl: cdk.Duration.minutes(1),
+        },
         {
           httpStatus: 404,
           responseHttpStatus: 200,
           responsePagePath: '/index.html',
-        },
-        {
-          httpStatus: 403,
-          responseHttpStatus: 200,
-          responsePagePath: '/index.html',
-        },
+          ttl: cdk.Duration.minutes(1),
+        }
       ],
     });
 
-    // Outputs
-    new cdk.CfnOutput(this, 'WebsiteURL', {
-      value: `https://${this.distribution.domainName}`,
+    // 4. Overwrite L2 OAI with L1 OAC (CDK L2 doesn't support OAC yet)
+    const cfnDist = distribution.node.defaultChild as cloudfront.CfnDistribution;
+    cfnDist.addPropertyOverride('DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity', '');
+    cfnDist.addPropertyOverride('DistributionConfig.Origins.0.OriginAccessControlId', oac.attrId);
+
+    // 5. Bucket Policy for OAC
+    websiteBucket.addToResourcePolicy(new cdk.aws_iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [websiteBucket.arnForObjects('*')],
+      principals: [new cdk.aws_iam.ServicePrincipal('cloudfront.amazonaws.com')],
+      conditions: {
+        StringEquals: {
+          'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+        },
+      },
+    }));
+
+    // 6. SSM Parameter for the Bucket Name (for CI/CD)
+    new ssm.StringParameter(this, 'PrajnaBucketNameParam', {
+      parameterName: '/prajna/bucket-name',
+      stringValue: websiteBucket.bucketName,
     });
+
+    new cdk.CfnOutput(this, 'WebsiteURL', { value: distribution.distributionDomainName });
   }
 }
